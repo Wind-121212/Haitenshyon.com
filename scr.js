668ab478dad3e5bcf748e453d54fd7b389074826
Wind-121212 <146910218+Wind-121212@users.noreply.github.com>
2024-01-10 20:08:42 +0700

    Add files via upload

---
+++ scr.js
@@ -0,0 +1,52 @@
+     let popup = document.getElementById("myForm");
+      let openbtn = document.getElementById("openbtn");
+      let grid = document.getElementById("grid");
+      function openForm(){
+     popup.classList.add("open-nav");
+     openbtn.classList.add("open-ative");
+     grid.classList.remove("grid-close");
+ }
+ function closeForm(){
+     popup.classList.remove("open-nav");
+     openbtn.classList.remove("open-ative");
+     grid.classList.add("grid-close");
+ }
+
+ const getLoction = () => {
+    if(navigator.geolocation.getCurrentPosition){
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    }
+    else
+    {
+        alert("This dvice dose not suport the future.")
+    }
+ };
+
+ const showPosition = (position) =>{
+    let lat = position.coords.latitude;
+    let long = position.coords.longitude;
+
+    const des = document.querySelector("p");
+     des.innerHTML = 'Latitude : ${lat} <br> Longitude : ${long}';
+    console.log(lat,long);
+}
+
+const showError = (error) => {
+    switch(error.code){
+        case error.PERRMISSOIN_DENIED:
+            alert("PERRMISSOIN DENIED");
+            break;
+
+        case error.POSITION_UNAVAILABLE:
+            alert("POSITION UNAVAILABLE");
+            break;
+            
+            case error.TIMEOUT:
+            alert("TIMEOUT");
+            break;
+
+            case error.UNKNOWN_ERROR:
+            alert("UNKNOWN ERROR");
+            break;
+    }
+}
